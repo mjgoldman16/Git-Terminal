6dc5270d1dd272565b9f757131bf3bedc1864595
Michael Goldman <mjgoldman16@gmail.com>
2018-01-09 16:45:49 -0500

    Restoring the poorly made red triangle

---
+++ triangle.py
@@ -0,0 +1,12 @@
+import turtle
+
+painter = turtle.Turtle()
+painter.pencolor("red")
+
+for _ in range(13):
+	painter.forward(100)
+	painter.left(140)
+	painter.backward(20)
+	painter.right(30) 
+
+turtle.done()
